0c2971b231d16222121a7e16a24c2edf85abc4ca
etunaa <104025100+etunaa@users.noreply.github.com>
2022-05-31 19:56:18 +0400

    2

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ var cinema={
     ]
 }
 
-let text=""
-for (var i =0; i<cinema.length; i++){
-    text=cinema[i] + "<br>";
-}
-document.getElementById("demo").innerHTML=text
+
+
+document.getElementById("demo1").innerHTML= "name:"+"  " +cinema.name
+document.getElementById("demo2").innerHTML= "languaje:" +"  "+cinema.languajes
+document.getElementById("demo3").innerHTML= "address:"+"  "+cinema.address
+
+for(var i=0; i<cinema.movies.length; i++)
+    document.write( "movie:"+"  "+cinema.movies[i].name +"</br>"+ "imdb:"+"  "+cinema.movies[i].imdb + "</br>")
+
+// for(var i=0; i<cinema.movies.length; i++){
+// document.getElementById("demo4").innerHTML=cinema.movies[i].name + cinema.movies[i].imdb}
